e5a153e4a2a19067b10c665f56b2239a156c785e
Muhammad Talha <khankkhan123apsacs@gmail.com>
2025-06-11 14:58:24 +0500

    rename: contants.rs -> resolutions.rs

---
+++ src/resolutions.rs
@@ -0,0 +1,18 @@
+/*
+Created By: Muhammad Talha (MTalha-Codes)
+File Name: resolutions
+Purpose: To store all xresm wide constants
+*/
+pub const RESOLUTION_640_480: (&str,&str) = ("640", "480");
+pub const RESOLUTION_800_600: (&str,&str) = ("800","600");
+pub const RESOLUTION_1024_768: (&str,&str) = ("1024","768");
+pub const RESOLUTION_1280_1024: (&str,&str) = ("1280","1024");
+pub const RESOLUTION_1280_800: (&str,&str) = ("1280","800");
+pub const RESOLUTION_1366_768: (&str,&str) = ("1366","768");
+pub const RESOLUTION_1600_900: (&str,&str) = ("1600","900");
+pub const RESOLUTION_1680_1050: (&str,&str) = ("1680","1050");
+pub const RESOLUTION_1920_1080: (&str,&str) = ("1920","1080");
+pub const RESOLUTION_1920_1200: (&str,&str) = ("1920","1200");
+pub const RESOLUTION_2560_1440: (&str,&str) = ("2560","1440");
+pub const RESOLUTION_2560_1600: (&str,&str) = ("2560","1600");
+pub const RESOLUTION_3840_2160:(&str,&str) = ("3840","2160");
