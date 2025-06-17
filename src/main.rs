11ba3b9251681c42ac3b479dde63724535257f28
Muhammad Talha <khankkhan123apsacs@gmail.com>
2025-06-17 22:42:42 +0500

    No Logic Changed

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod resolution_manager;
 mod resolutions;
 
 fn main() {
-    use resolutions::RESOLUTION_1680_1050;
-    let (w, h) = (RESOLUTION_1680_1050.0, RESOLUTION_1680_1050.1);
-    let resm = resolution_manager::ResolutionManager::new(w, h);
-    resm.apply_resolution();
+    let (w,h) = resolutions::RESOLUTION_1680_1050;
+    let resm = resolution_manager::ResolutionManager::new(w,h,"VGA-1");
+    match resm.apply_resolution(){
+        Err(error) => println!("{error}"),
+        Ok(success) => println!("{success}"),
+    }; 
 }
